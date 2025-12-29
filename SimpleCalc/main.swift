fa07796a00aec14c3f117d19e4867a282a4e49a7
n0lit <n0rthl1ght@yandex.ru>
2025-12-30 02:06:56 +0300

    init comit create base initicalize() func

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
-
+func initicalize() {
+    print("v0.1")
+    print("Wellcome to SimpleCalc")
+    print("Select operation type: +, -, *, /")
+    let operandInt:String = readLine() ?? " "
+    switch operandInt{
+    case "+":
+        
+    case "-":
+        
+    case "*":
+        
+    case "/":
+        
+    default:
+        print("Input Error")
+    }
+}
