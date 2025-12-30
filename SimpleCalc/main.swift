01427de2d847a846833c215594a77c0d219e6b8d
n0lit <n0rthl1ght@yandex.ru>
2025-12-30 21:57:10 +0300

    the function of addition of two Int has been implemented minor changes

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -1,27 +1,42 @@
-//
-//  main.swift
-//  SimpleCalc
-//
-//  Created by Максим Фурсов on 12/30/25.
-//
+public let version:String = "v0.13"
 
 import Foundation
 
+func plus(number1:String, number2:String) -> Int{
+    if let n1 = Int(number1){
+        if  let n2 = Int(number2){
+            return n1+n2
+        }else{
+            print("Unexepted error: Check your second number")
+        }
+    }else{
+        print("Unexepted error: Check your first number")
+    }
+    return 0
+}
+
 func initicalize() {
-    print("v0.1")
+    print(version)
     print("Wellcome to SimpleCalc")
     print("Select operation type: +, -, *, /")
-    let operandInt:String = readLine() ?? " "
+    let operandInt:String = readLine() ?? ""
+    print("Furst number:")
+    let n1:String = readLine() ?? ""
+    print("Second number:")
+    let n2:String = readLine() ?? ""
+    print("Your expression: " + n1 + " " + operandInt + " " + n2)
     switch operandInt{
     case "+":
-        
+        print("Result: " + String(plus(number1: n1, number2: n2)))
     case "-":
-        
+        print("test -")
     case "*":
-        
+        print("test *")
     case "/":
-        
+        print("test /")
     default:
         print("Input Error")
     }
 }
+
+initicalize()
