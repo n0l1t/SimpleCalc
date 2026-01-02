772e62cd9624fe5f50fe98d0a63887723afc41d1
n0lit <n0rthl1ght@yandex.ru>
2026-01-02 19:04:34 +0300

    refactoring main function add subtraction minor changes

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -1,42 +1,42 @@
-public let version:String = "v0.13"
-
 import Foundation
 
-func plus(number1:String, number2:String) -> Int{
-    if let n1 = Int(number1){
-        if  let n2 = Int(number2){
-            return n1+n2
+public let version:String = "v0.2"
+
+func calcFunc(number1:String, number2:String, operand:String) -> Int{
+    switch operand {
+    case "+":
+        if let n1 = Int(number1){
+            if  let n2 = Int(number2){
+                return n1+n2
+            }else{
+                print("Unexepted error: Check your second number")
+            }
         }else{
-            print("Unexepted error: Check your second number")
+            print("Unexepted error: Check your first number")
         }
-    }else{
-        print("Unexepted error: Check your first number")
+    case "-":
+        if let n1 = Int(number1){
+            if  let n2 = Int(number2){
+                return n1-n2
+            }else{
+                print("Unexepted error: Check your second number")
+            }
+        }else{
+            print("Unexepted error: Check your first number")
+        }
+    default: print("Unexepted error")
     }
-    return 0
+ return 0
 }
 
 func initicalize() {
-    print(version)
-    print("Wellcome to SimpleCalc")
+    print("Wellcome to SimpleCalc " + version)
     print("Select operation type: +, -, *, /")
     let operandInt:String = readLine() ?? ""
     print("Furst number:")
     let n1:String = readLine() ?? ""
     print("Second number:")
     let n2:String = readLine() ?? ""
-    print("Your expression: " + n1 + " " + operandInt + " " + n2)
-    switch operandInt{
-    case "+":
-        print("Result: " + String(plus(number1: n1, number2: n2)))
-    case "-":
-        print("test -")
-    case "*":
-        print("test *")
-    case "/":
-        print("test /")
-    default:
-        print("Input Error")
-    }
+    print("Your expression: " + n1 + " " + operandInt + " " + n2 + " = " + String(calcFunc(number1: n1, number2: n2, operand:operandInt)))
 }
-
 initicalize()
